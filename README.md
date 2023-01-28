6db6a19325df6ab407e687fce358600c2bc64343
gowtham <medagowtham2@yahoo.com>
2023-01-28 15:40:15 +0530

    for and splat

--- README.md
+++ README.md
@@ -9,3 +9,50 @@ A program may need mutliple provider may its ia hashicorp or 3rd party like null
 you first initiliaze the tf with the terraform block whick has the info about whick specifies the source and the verison you want to use
 
 and provider block provides the addiition info about the region and other 
+
+
+
+//to get the  meta data from aws console
+curl http://169.254.169.254/latest/meta-data/public-ipv4 
+
+//to see the state file better
+sudo apt install jq
+
+//to view the state file with jq
+terraform show -json | jq
+
+//to list the resources deployed
+terraform state list
+
+// to list the state file
+terraform show
+
+//to parse the state file show the required file,
+where it show the all the names that are in that are in the state file
+terraform show | name
+
+//terraform cli
+$ terraform console
+
+<resource name>.<resource identifier>.<the flied you want to see like name,port,ip>
+
+join(seperator, list)
+join("-" [${name},db])
+tfdemo-db
+
+array{
+
+    key1 = value1
+    key2 = value2
+}
+
+to use as a reference you need to add index as an arry can be mutiple 
+like root block device ther  may be many so to differtiate programs use indexing
+docker_container.node_red.array[0].key
+
+join(":", [docker_container.nodered.network_data[0].ip_address,docker_container.nodered.ports[0].internal])
+
+join(":", [docker_container.nodered.network_data[0].ip_address,docker_container.nodered.ports[0].internal])
+
+container_ip-address_port = "172.17.0.2:1880"
+http_container_ip-address_port = "http:\\172.17.0.2:1880"
--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "3.0.1"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
   }
 }
 
@@ -14,5 +18,66 @@ provider "docker" {
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
-
 }
+
+resource "random_string" "random" {
+  count   = 2
+  length  = 4
+  special = false
+  upper   = false
+}
+
+resource "docker_container" "nodered" {
+  count = 2
+  name  = "nodered-container-${random_string.random[count.index].id}"
+  image = docker_image.nodered_image.image_id
+  ports {
+    internal = 1880
+    #external = 1880
+  }
+}
+
+output "container_ids" {
+  value = docker_container.nodered[*].name
+}
+
+output "container_ip-address_port" {
+  value = join(":", [docker_container.nodered[0].network_data[0].ip_address, docker_container.nodered[0].ports[0].external])
+}
+
+output "http_container_ip-address_port" {
+  value = join(":", ["http:\\${docker_container.nodered[1].network_data[0].ip_address}", docker_container.nodered[1].ports[0].external])
+}
+
+output "container_ports" {
+  value = docker_container.nodered[*].ports
+}
+
+output "container_ip_with_for" {
+  value = [for i in docker_container.nodered[*] : join(":", [i.network_data[0].ip_address, i.ports[0].external])]
+}
+
+//splat operator shoes the op in differnt list where as indexing shows in the same list
+output "container_ip_addr_with_splat" {
+  value = [for i in docker_container.nodered[*] : i.network_data[*].ip_address]
+}
+
+output "container_ip_addr_with_index" {
+  value = [for i in docker_container.nodered[*] : i.network_data[0]["ip_address"]]
+}
+
+
+
+# resource "docker_container" "nodered2" {
+#   name  = join ("-", ["nodered-container2",random_string.random.id]) 
+#   image = docker_image.nodered_image.image_id
+#   ports {
+#     internal = 1880
+#     #external = 1880
+#   }
+# }
+
+# output "http_container_ip-address_port" {
+#   value = join(":", ["http:\\${docker_container.nodered.network_data[0].ip_address}", docker_container.nodered2.ports[0].external])
+# }
+
